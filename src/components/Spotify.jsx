e0d21a7b8ef2528fea612a2faa2ee778ad37df5e
Luis <zenitram40@yahoo.com>
2023-10-07 14:42:13 -0700

    login with spotify and access token from spotify

---
+++ src/components/Spotify.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Spotify() {
+    return (
+        <Container>
+            <div className='spotify_body'>
+                <div className='body'>
+                    <div className='body_contents'></div>
+                </div>
+            </div>
+            <div className='spotify_footer'></div>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+
+`
+
