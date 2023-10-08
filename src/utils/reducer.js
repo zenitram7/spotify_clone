8593b25c9990bd9dd4c39b7d818e9fc5e67bdd32
Luis <zenitram40@yahoo.com>
2023-10-07 17:22:42 -0700

    navbar/search bar

--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -3,6 +3,7 @@ import { reducerCases } from "./Constants";
 export const initialState = {
     token: null,
     playlists: [],
+    userInfo: null,
 };
 
 const reducer = (state, action) => {
@@ -16,6 +17,12 @@ const reducer = (state, action) => {
             return {
                 ...state,
                 playlists: action.playlists,
+            }
+        }
+        case reducerCases.SET_USER: {
+            return {
+                ...state,
+                userInfo: action.userInfo,
             }
         }
         default:
