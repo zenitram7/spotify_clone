ab9520dd11b342730b347ef5210acc8bfdd682f5
Luis <zenitram40@yahoo.com>
2023-10-12 06:32:30 -0700

    change track in body functionality/ spotify API broken

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -7,6 +7,7 @@ import { reducerCases } from '../utils/Constants';
 
 export default function Body({ headerBackground }) {
     const [{ token, selectedPlaylistId, selectedPlaylist }, dispatch] = useStateProvider();
+    console.log(token)
     useEffect(() => {
         const getInitialPlaylist = async () => {
             const response = await axios.get(
@@ -37,12 +38,47 @@ export default function Body({ headerBackground }) {
         };
         getInitialPlaylist();
     }, [token, dispatch, selectedPlaylistId]);
+
+    const playTrack = async (id, name, artists, image, context_uri, track_number) => {
+        try {
+            const response = await axios.put(
+                `https://api.spotify.com/v1/me/player/play`,
+                {
+                    context_uri,
+                    offset: {
+                        position: track_number - 1,
+                    },
+                    position_ms: 0,
+                },
+                {
+                    headers: {
+                        Authorization: "Bearer " + token,
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+            if (response.status === 204) {
+                const currentlyPlaying = {
+                    id,
+                    name,
+                    artists,
+                    image,
+                };
+                dispatch({ type: reducerCases.SET_PLAYING, currentlyPlaying });
+                dispatch({ type: reducerCases.SET_PLAYER_STATE, playerState: true });
+            } else {
+                dispatch({ type: reducerCases.SET_PLAYER_STATE, playerState: true });
+            }
+        } catch (error) {
+            console.log('Error playing the track:', error);
+        }
+    };
+
     const msToMinutesAndSeconds = (ms) => {
         const minutes = Math.floor(ms / 60000);
         const seconds = ((ms % 60000) / 1000).toFixed(0);
         return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
-    };
-
+    }
     return (
         <Container headerBackground={headerBackground}>
             {selectedPlaylist && (
@@ -86,7 +122,12 @@ export default function Body({ headerBackground }) {
                                 track_number,
                             }, index) => {
                                 return (
-                                    <div className='row' key={id}>
+                                    <div className='row' key={id} onClick={() => playTrack(id,
+                                        name,
+                                        artists,
+                                        image,
+                                        context_uri,
+                                        track_number)}>
                                         <div className='col'>
                                             <span>{index + 1}</span>
                                         </div>
--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -23,14 +23,19 @@ export default function Playlist() {
             dispatch({ type: reducerCases.SET_PLAYLISTS, playlists });
         };
         getPlaylistData();
-    }, [token, dispatch])
+    }, [token, dispatch]);
+
+    const changeCurrentPlaylist = (selectedPlaylistId) => {
+        dispatch({ type: reducerCases.SET_PLAYLIST_ID, selectedPlaylistId });
+
+    }
     return (
         <Container>
             <ul>
                 {
                     playlists.map(({ name, id }) => {
                         return (
-                            <li key={id}>{name}</li>
+                            <li key={id} onClick={() => changeCurrentPlaylist(id)}>{name}</li>
                         )
                     })
                 }
